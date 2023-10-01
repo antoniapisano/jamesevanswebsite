233c1a71090c3ce3f80676cdc05ea9ab9d54cb6e
antoniapisano <antonia.a.pisano@gmail.com>
2023-10-01 17:50:00 +0100

    now to sort out footer

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,20 @@ export default function Home() {
   return (
     
     <div className={styles.container}>
-      <Head>
-        <title>James Evans Official Website UK Blues Artist</title>
-      </Head>
-      <div className={styles.youtubecontainer}>
-        <YoutubeEmbed embedId="wp58tHwugI8" />
-          </div>
-            
+    <Head>
+      <title>James Evans Official Website UK Blues Artist</title>
+    </Head>
+
+   <div className={styles.gridcontainer}>
+      <div className={styles.headcontainer}>
+        <h1 className={styles.thistitle}>James <br className={styles.brtitle}/>Evans</h1>
+      </div>
+    <div className={styles.youtubecontainer}>
+      <YoutubeEmbed embedId="wp58tHwugI8" />
+        </div>
+
+    </div>
+  
 
     
     
